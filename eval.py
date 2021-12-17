227ab0225290e0fac25046920e97a03f53e0efc6
abamigbade <95593166+abamigbade@users.noreply.github.com>
2021-12-17 07:19:17 +0400

    Update eval.py

--- eval.py
+++ eval.py
@@ -12,12 +12,12 @@ def data_loader(filepath):
     data = h5py.File(filepath, 'r')
     x_data = np.array(data['data'])
     x_data = x_data.transpose((0,2,3,1))
-
-    return x_data, y_data
+    
+    return x_data
 
 def main():
-    cl_x_test, cl_y_test = data_loader(CleanData_filename)
-    bd_x_test, bd_y_test = data_loader(BackdooredData_filename)
+    cl_x_test = data_loader(CleanData_filename)
+    bd_x_test = data_loader(BackdooredData_filename)
 
     BadNet = keras.models.load_model(BadNet_filename)
     RepairedNet = keras.models.load_model(RepairedNet_filename)
@@ -25,8 +25,10 @@ def main():
     bd_label_p = np.argmax(BadNet.predict(bd_x_test), axis=1)
     cl_label_p = np.argmax(RepairedNet.predict(cl_x_test), axis=1)
     
+    ########################################################
     # Specify the index of the dataset to evaluate
     i = 100 # A default index of 100 is used here
+    ########################################################
     
     if bd_label_p[i] == cl_label_p[i]:
         test_data_label = cl_label_p[i]
